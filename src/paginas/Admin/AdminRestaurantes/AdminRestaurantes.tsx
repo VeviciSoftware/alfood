87284995b6240dd4ad2ea9846f35b125e72bcbe4
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-07-10 11:37:48 -0300

    :sparkles: feat: crud de restaurantes completo

--- src/paginas/Admin/AdminRestaurantes/AdminRestaurantes.tsx
+++ src/paginas/Admin/AdminRestaurantes/AdminRestaurantes.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
-import { Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { Paper, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Button } from '@mui/material';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 const AdministracaoRestaurantes = () => {
@@ -16,7 +17,18 @@ const AdministracaoRestaurantes = () => {
             .catch(error => {
                 console.log(error);
             });
-    })
+    }, []);
+
+    const excluir = (restauranteDeletar: IRestaurante) => {
+        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteDeletar.id}/`)
+            .then(() => {
+                const listRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteDeletar.id);
+                setRestaurantes(listRestaurante);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
 
     return (
         <TableContainer component={Paper}>
@@ -24,12 +36,23 @@ const AdministracaoRestaurantes = () => {
                 <TableHead>
                     <TableRow>
                         <TableCell>Nome</TableCell>
+                        <TableCell>Editar</TableCell>
+                        <TableCell>Excluir</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {restaurantes.map((restaurante) => (
                         <TableRow key={restaurante.id}>
                             <TableCell>{restaurante.nome}</TableCell>
+                            <TableCell>
+                                <Link to={`/admin/restaurantes/${restaurante.id}`}>Editar</Link> 
+                            </TableCell>
+                            <TableCell>
+                                <Button variant="contained" color="error" onClick={() => excluir(restaurante)}>
+                                    Excluir
+                                </Button>
+
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
