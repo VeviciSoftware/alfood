25783cb27dc697a724baa33766532614f03a09c4
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-07-17 11:48:46 -0300

    :sparkles: feat: edição de pratos

--- src/paginas/Admin/AdminPratos/FormularioPratos.tsx
+++ src/paginas/Admin/AdminPratos/FormularioPratos.tsx
@@ -1,46 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import http from '../../../http';
+import ITag from '../../../interfaces/ITag';
+import IRestaurante from '../../../interfaces/IRestaurante';
 import {
-  Box,
-  Button,
-  Container,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Paper,
-  Select,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useEffect, useState } from "react";
-import http from "../../../http";
-import ITag from "../../../interfaces/ITag";
-import IRestaurante from "../../../interfaces/IRestaurante";
+    Box,
+    Button,
+    Container,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Paper,
+    Select,
+    TextField,
+    Typography,
+  } from "@mui/material";
 
 const FormularioPratos = () => {
+  const { id } = useParams<{ id: string }>();
   const [nomePrato, setNomePrato] = useState("");
   const [descricao, setDescricao] = useState("");
-
   const [tag, setTag] = useState("");
   const [tags, setTags] = useState<ITag[]>([]);
-
   const [restaurante, setRestaurante] = useState("");
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
-
   const [imagem, setImagem] = useState<File | null>(null);
 
   useEffect(() => {
-    http
-      .get<{ tags: ITag[] }>("tags/")
+    http.get<{ tags: ITag[] }>("tags/")
       .then((response) => setTags(response.data.tags));
-    http
-      .get<IRestaurante[]>("restaurantes/")
+    http.get<IRestaurante[]>("restaurantes/")
       .then((response) => setRestaurantes(response.data));
-  }, []);
+
+    if (id) {
+      http.get(`pratos/${id}/`)
+        .then((response) => {
+          setNomePrato(response.data.nome);
+          setDescricao(response.data.descricao);
+          setTag(response.data.tag);
+          setRestaurante(response.data.restaurante);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [id]);
+
+  const selecionarArquivo = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    if (evento.target.files) {
+      setImagem(evento.target.files[0]);
+    } else {
+      setImagem(null);
+    }
+  };
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
 
     const formData = new FormData();
-
     formData.append("nome", nomePrato);
     formData.append("descricao", descricao);
     formData.append("tag", tag);
@@ -50,33 +67,28 @@ const FormularioPratos = () => {
       formData.append("imagem", imagem);
     }
 
+    const metodo = id ? "PUT" : "POST";
+    const url = id ? `pratos/${id}/` : "pratos/";
+
     http.request({
-        url: "pratos/",
-        method: "POST",
-        headers: {
-            "Content-Type": "multipart/form-data",
-            },
-        data: formData,
+      url: url,
+      method: metodo,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: formData,
     })
     .then(() => {
-        setNomePrato("");
-        setDescricao("");
-        setTag("");
-        setRestaurante("");
-        setImagem(null);
-        alert("Prato cadastrado com sucesso!");
+      setNomePrato("");
+      setDescricao("");
+      setTag("");
+      setRestaurante("");
+      setImagem(null);
+      alert(`Prato ${id ? "atualizado" : "cadastrado"} com sucesso!`);
     })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     });
-  };
-
-  const selecionarArquivo = (evento: React.ChangeEvent<HTMLInputElement>) => {
-    if (evento.target.files) {
-      setImagem(evento.target.files[0]);
-    } else {
-      setImagem(null);
-    }
   };
 
   return (
